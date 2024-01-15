d480aeea0370854dc7c1618101409d4460ad8305
aleksandrskatanskis <144622436+aleksandrskatanskis@users.noreply.github.com>
2024-01-15 13:38:51 +0200

    Add files via upload

---
+++ projekt.py
@@ -0,0 +1,50 @@
+import os
+import random
+from docx.shared import Cm, Pt
+from docx import Document
+
+spiskiDirectory = 'spiski/'
+protokoliDirectory = 'protokoli/'
+
+def createWordFromText(textFilePath):
+    with open(textFilePath, 'r') as file:
+        content = file.readlines()
+
+    random.shuffle(content)
+
+    doc = Document()
+
+    headingTxt = os.path.splitext(os.path.basename(textFilePath))[0]
+    heading = doc.add_heading(level=1)
+    run = heading.add_run(headingTxt)
+    run.font.size = Pt(30)
+
+    table = doc.add_table(rows=0, cols=4, style='TableGrid')
+
+    for index, line in enumerate(content):
+        values = line.strip().split(',')
+
+        row = table.add_row().cells
+
+        row[1].text = str(index + 1)  
+        row[2].text = values[0].strip()  
+        row[3].text = values[1].strip() 
+     
+
+    for i in range(0, 4):
+        for col in table.columns[i].cells:
+            col.width = Cm(0.8)
+            for paragraph in col.paragraphs:
+                for run in paragraph.runs:
+                    run.font.size = Pt(24)
+          
+   
+
+    filename = os.path.splitext(os.path.basename(textFilePath))[0] + '.docx'
+    doc.save(os.path.join(protokoliDirectory, filename))
+
+textFiles = os.listdir(spiskiDirectory)
+
+for filename in textFiles:
+    textFilePath = os.path.join(spiskiDirectory, filename)
+    createWordFromText(textFilePath)
